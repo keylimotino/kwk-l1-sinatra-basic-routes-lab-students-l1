540d7f623cbcebb0707ec5538260bf36e66e4a47
keylimotino <stephanie48758@gmail.com>
2018-06-12 15:50:00 +0000

    automatically backed up by learn

--- app.rb
+++ app.rb
@@ -6,11 +6,11 @@ class App < Sinatra::Base
   end
 
   get '/hometown' do
-   "My hometown is New Orleans}"
+   "My hometown is New Orleans"
   end
 
   get '/favorite-song' do
-    "My favorite-song is blank"
+    "My favorite song is Nice for what"
   end
 
 end
